2081c0a794df7ca9dd2fadd4cba4a6ee4ebd1bb0
Nirattasai <nirattasai.h@ku.th>
2020-09-21 17:51:34 +0700

    Create application space

--- src/main/java/Final/Main.java
+++ src/main/java/Final/Main.java
@@ -10,9 +10,10 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("../../resources/sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/Welcome.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 1920, 1020));
+        primaryStage.setResizable(false);
         primaryStage.show();
     }
 
